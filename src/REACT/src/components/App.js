30091b4ab3441d323f1e820441cbc1812818a344
Hervé Duchesne <hduchesne@jahia.com>
2020-09-01 18:32:46 +0200

    Start to create the UI to search an asset...

--- src/REACT/src/components/App.js
+++ src/REACT/src/components/App.js
@@ -1,24 +1,85 @@
 import React from 'react';
 import logo from 'assets/logo.svg';
 import 'components/App.scss';
+import axios from 'axios';
+import {Form} from "react-bootstrap";
 
-function App() {
+const App = (props) => {
+
+    const [query, setQuery] = React.useState("dog");
+    const [data, setData] = React.useState({ });
+  const [iframe, setIframe] = React.useState();
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const categories = await axios(
+          'https://api.widencollective.com/v2/categories',
+          {
+            headers:{
+              Authorization: 'Bearer virbac/ba4d0a71907a17aff9ebddc1fc91fd3a'
+            }
+          }
+      );
+      console.log("categories : ",categories)
+      setData(categories.data);
+    };
+
+    fetchData();
+
+  },[]);
+
+    React.useEffect(() => {
+        const fetchData = async () => {
+            const instantSearch = await axios(
+                'https://api.widencollective.com/v2/integrations/url',
+                {
+                    headers: {
+                        Authorization: 'Bearer virbac/ba4d0a71907a17aff9ebddc1fc91fd3a'
+                    },
+                    params: {
+                        //hideSearchBar: true,
+                        query: query
+                    }
+                }
+            );
+
+            console.log("instantSearch : ", instantSearch.data);
+            //const frameURL = URL.createObjectURL(instantSearch.data);
+            setIframe(instantSearch.data.url);
+        };
+        fetchData();
+    },[query])
+
+    window.addEventListener('message', event =>{
+        console.log("message : ",event.data);
+        // const embedCode = event.data.items[0].embed_code;
+        // console.log(embedCode);
+
+        // Insert embedCode into user’s html content
+        // Close iframe
+    })
+
+  const handleChange = (e) =>{
+      const value= e.target.value;
+      console.log("value : ",value);
+      //TODO set query
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <Form.Control
+              name="search"
+              id="search_123"
+              label="Search"
+              onChange={handleChange}
+          />
+        {iframe &&
+        <iframe src={iframe} width="70%" height="700px"></iframe>
+        }
       </header>
+
+
     </div>
   );
 }
