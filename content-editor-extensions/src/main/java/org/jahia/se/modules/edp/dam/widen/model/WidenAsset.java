42e9b1a26f48fe144ba0e7e0f11984038ea67e68
Hervé Duchesne <hduchesne@jahia.com>
2020-12-15 20:10:42 +0100

    v8.0.1.4 OSGI Working JSON mapping
    TODO see if I can use OSGI config stuff

---
+++ content-editor-extensions/src/main/java/org/jahia/se/modules/edp/dam/widen/model/WidenAsset.java
@@ -0,0 +1,230 @@
+package org.jahia.se.modules.edp.dam.widen.model;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@JsonDeserialize(using = WidenAssetDeserializer.class)
+public class WidenAsset {
+
+    private String jahiaNodeType;
+    private final Map<String, Object> properties;
+
+    public WidenAsset(){
+        properties=new HashMap<>();
+    }
+
+    public String getJahiaNodeType() {
+        return jahiaNodeType;
+    }
+
+    public void setJahiaNodeType(String jahiaNodeType) {
+        this.jahiaNodeType = jahiaNodeType;
+    }
+
+    public Map<String, Object> getProperties() {
+        return properties;
+    }
+
+    public void addProperties(String name,Object value){
+        properties.put(name, value);
+    }
+
+
+//    private String id;
+//    private String externalId;
+//    private String filename;
+//    private String createdDate;
+//    private String updatedDate;
+//    private String deletedDate;
+//    private String thumbnail;
+//    //fileProperties
+//    private String format;
+//    private String type;
+//    private Long sizeKB;
+//    //imageFileProperties
+//    private Double width;
+//    private Double height;
+//    private Double aspectRatio;
+//    //videoFileProperties
+//    private Double duration;
+//    //embed
+//    private String templatedUrl;
+//    //embedVideo
+//    private String videoPlayer;
+//    private String videoStreamURL;
+//    private String videoStreamHTML;
+//    private String videoPoster;
+//    //embedPdf
+//    private String viewerHtml5;
+//    private String viewer;
+//    private String docThumbnail;
+//    private String docURL;
+//    private String docHTMLLink;
+
+
+
+//
+//    public String getId() { return id; }
+//    public void setId(String id){ this.id = id;}
+//
+//    public String getExternalId() { return externalId; }
+//    public void setExternalId(String eid){ this.externalId = eid;}
+//
+//    public String getFilename() { return filename; }
+//    public void setFilename(String filename){ this.filename = filename;}
+//
+//    public String getCreatedDate() { return createdDate; }
+//    public void setCreatedDate(String createdDate){ this.createdDate = createdDate;}
+//
+//    public String getUpdatedDate() { return updatedDate; }
+//    public void setUpdatedDate(String updatedDate){ this.updatedDate = updatedDate;}
+//
+//    public String getDeletedDate() { return deletedDate; }
+//    public void setDeletedDate(String deletedDate){ this.deletedDate = deletedDate;}
+//
+//    public String getThumbnail() {
+//        return thumbnail;
+//    }
+//
+//    public String getFormat() {
+//        return format;
+//    }
+//
+//    public String getType() {
+//        return type;
+//    }
+//
+//    public Long getSizeKB() {
+//        return sizeKB;
+//    }
+//
+//    public Double getWidth() {
+//        return width;
+//    }
+//
+//    public Double getHeight() {
+//        return height;
+//    }
+//
+//    public Double getAspectRatio() {
+//        return aspectRatio;
+//    }
+//
+//    public Double getDuration() {
+//        return duration;
+//    }
+//
+//    public String getTemplatedUrl() {
+//        return templatedUrl;
+//    }
+//
+//    public String getVideoPlayer() {
+//        return videoPlayer;
+//    }
+//
+//    public String getVideoStreamURL() {
+//        return videoStreamURL;
+//    }
+//
+//    public String getVideoStreamHTML() {
+//        return videoStreamHTML;
+//    }
+//
+//    public String getVideoPoster() {
+//        return videoPoster;
+//    }
+//
+//    public String getViewerHtml5() {
+//        return viewerHtml5;
+//    }
+//
+//    public String getViewer() {
+//        return viewer;
+//    }
+//
+//    public String getDocThumbnail() {
+//        return docThumbnail;
+//    }
+//
+//    public String getDocURL() {
+//        return docURL;
+//    }
+//
+//    public String getDocHTMLLink() {
+//        return docHTMLLink;
+//    }
+//
+//    public void setThumbnail(String thumbnail) {
+//        this.thumbnail = thumbnail;
+//    }
+//
+//    public void setFormat(String format) {
+//        this.format = format;
+//    }
+//
+//    public void setType(String type) {
+//        this.type = type;
+//    }
+//
+//    public void setSizeKB(Long sizeKB) {
+//        this.sizeKB = sizeKB;
+//    }
+//
+//    public void setWidth(Double width) {
+//        this.width = width;
+//    }
+//
+//    public void setHeight(Double height) {
+//        this.height = height;
+//    }
+//
+//    public void setAspectRatio(Double aspectRatio) {
+//        this.aspectRatio = aspectRatio;
+//    }
+//
+//    public void setDuration(Double duration) {
+//        this.duration = duration;
+//    }
+//
+//    public void setTemplatedUrl(String templatedUrl) {
+//        this.templatedUrl = templatedUrl;
+//    }
+//
+//    public void setVideoPlayer(String videoPlayer) {
+//        this.videoPlayer = videoPlayer;
+//    }
+//
+//    public void setVideoStreamURL(String videoStreamURL) {
+//        this.videoStreamURL = videoStreamURL;
+//    }
+//
+//    public void setVideoStreamHTML(String videoStreamHTML) {
+//        this.videoStreamHTML = videoStreamHTML;
+//    }
+//
+//    public void setVideoPoster(String videoPoster) {
+//        this.videoPoster = videoPoster;
+//    }
+//
+//    public void setViewerHtml5(String viewerHtml5) {
+//        this.viewerHtml5 = viewerHtml5;
+//    }
+//
+//    public void setViewer(String viewer) {
+//        this.viewer = viewer;
+//    }
+//
+//    public void setDocThumbnail(String docThumbnail) {
+//        this.docThumbnail = docThumbnail;
+//    }
+//
+//    public void setDocURL(String docURL) {
+//        this.docURL = docURL;
+//    }
+//
+//    public void setDocHTMLLink(String docHTMLLink) {
+//        this.docHTMLLink = docHTMLLink;
+//    }
+}
---
+++ content-editor-extensions/src/main/java/org/jahia/se/modules/edp/dam/widen/model/WidenAssetDeserializer.java
@@ -0,0 +1,187 @@
+package org.jahia.se.modules.edp.dam.widen.model;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+
+import java.io.IOException;
+
+public class WidenAssetDeserializer extends StdDeserializer<WidenAsset> {
+    private static final String PREFIX = "wden:";
+    private static final String THUMBNAIL_SIZE_KEY = "300px";
+
+    private static final String FILE_TYPE_IMAGE = "image";
+    private static final String FILE_TYPE_VIDEO = "video";
+    private static final String FILE_TYPE_PDF = "pdf";
+
+    private static final String CONTENT_TYPE_IMAGE = "wdennt:image";
+    private static final String CONTENT_TYPE_VIDEO = "wdennt:video";
+    private static final String CONTENT_TYPE_PDF = "wdennt:pdf";
+    private static final String CONTENT_TYPE_DOC = "wdennt:document";
+
+
+//    public WidenAssetDeserializer() {
+//        this(null);
+//    }
+
+    public WidenAssetDeserializer(Class<?> vc) {
+        super(vc);
+    }
+
+    @Override
+    public WidenAsset deserialize(JsonParser jsonParser, DeserializationContext deserializationContext)
+        throws IOException, JsonProcessingException {
+        JsonNode widenNode = jsonParser.getCodec().readTree(jsonParser);
+        WidenAsset widenAsset = new WidenAsset();
+
+        widenAsset.addProperties(PREFIX+"id",widenNode.get("id").textValue());
+        widenAsset.addProperties(PREFIX+"externalId",widenNode.get("external_id").textValue());
+        widenAsset.addProperties(PREFIX+"filename",widenNode.get("filename").textValue());
+        widenAsset.addProperties(PREFIX+"createdDate",widenNode.get("created_date").textValue());
+        widenAsset.addProperties(PREFIX+"updatedDate",widenNode.get("last_update_date").textValue());
+        widenAsset.addProperties(PREFIX+"deletedDate",widenNode.get("deleted_date").textValue());
+
+        widenAsset.addProperties(PREFIX+"templatedUrl",widenNode.get("embeds").get("templated").get("url").textValue());
+
+        if(widenNode.get("thumbnails").get(THUMBNAIL_SIZE_KEY)!=null)
+            widenAsset.addProperties(PREFIX+"thumbnail",widenNode.get("thumbnails").get(THUMBNAIL_SIZE_KEY).get("url").textValue());
+
+//        widenAsset.setId(widenNode.get("id").textValue());
+//        widenAsset.setExternalId(widenNode.get("external_id").textValue());
+//        widenAsset.setFilename(widenNode.get("filename").textValue());
+//        widenAsset.setCreatedDate(widenNode.get("created_date").textValue());
+//        widenAsset.setUpdatedDate(widenNode.get("last_update_date").textValue());
+//        widenAsset.setDeletedDate(widenNode.get("deleted_date").textValue());
+//
+//        widenAsset.setTemplatedUrl(widenNode.get("embeds").get("templated").get("url").textValue());
+//
+//        if(widenNode.get("thumbnails").get(THUMBNAIL_SIZE_KEY)!=null)
+//            widenAsset.setThumbnail(widenNode.get("thumbnails").get(THUMBNAIL_SIZE_KEY).get("url").textValue());
+
+        JsonNode filePropsNode = widenNode.get("file_properties");
+        String fileType = filePropsNode.get("format_type").textValue();
+        widenAsset.addProperties(PREFIX+"format",filePropsNode.get("format").textValue());
+        widenAsset.addProperties(PREFIX+"type",fileType);
+        widenAsset.addProperties(PREFIX+"sizeKB",filePropsNode.get("size_in_kbytes").textValue());
+
+//        widenAsset.setFormat(filePropsNode.get("format").textValue());
+//        widenAsset.setType(fileType);
+//        widenAsset.setSizeKB(filePropsNode.get("size_in_kbytes").longValue());
+
+        switch (fileType){
+            case FILE_TYPE_IMAGE :
+                widenAsset.setJahiaNodeType(CONTENT_TYPE_IMAGE);
+                extendFileProps(filePropsNode,widenAsset,"image_properties");
+
+//                JsonNode imageProps = widenNode.get("file_properties").get("image_properties");
+//                if(!imageProps.isNull()){
+//                    Double width = imageProps.get("width").doubleValue();
+//                    Double height = imageProps.get("height").doubleValue();
+//                    Double aspectRatio = imageProps.get("aspect_ratio").doubleValue();
+//
+//                    if(!width.isNaN())
+//                        widenAsset.setWidth(width);
+//                    if(!height.isNaN())
+//                        widenAsset.setHeight(height);
+//                    if(!aspectRatio.isNaN())
+//                        widenAsset.setAspectRatio(aspectRatio);
+//                }
+                break;
+            case FILE_TYPE_VIDEO:
+                widenAsset.setJahiaNodeType(CONTENT_TYPE_VIDEO);
+                extendFileProps(filePropsNode,widenAsset,"video_properties");
+
+                JsonNode playerNode = widenNode.get("embeds").get("video_player");
+                JsonNode streamNode = widenNode.get("embeds").get("video_stream");
+                JsonNode posterNode = widenNode.get("embeds").get("video_poster");
+
+                if(!playerNode.isNull())
+                    widenAsset.addProperties(PREFIX+"videoPlayer",playerNode.get("url").textValue());
+//                    widenAsset.setVideoPlayer(playerNode.get("url").textValue());
+
+                if(!streamNode.isNull()){
+                    widenAsset.addProperties(PREFIX+"videoStreamURL",streamNode.get("url").textValue());
+                    widenAsset.addProperties(PREFIX+"videoStreamHTML",streamNode.get("html").textValue());
+
+//                    widenAsset.setVideoStreamURL(streamNode.get("url").textValue());
+//                    widenAsset.setVideoStreamHTML(streamNode.get("html").textValue());
+                 }
+
+                if(!posterNode.isNull())
+                    widenAsset.addProperties(PREFIX+"videoPoster",posterNode.get("url").textValue());
+//                    widenAsset.setVideoPlayer(posterNode.get("url").textValue());
+
+//                JsonNode videoProps = widenNode.get("file_properties").get("video_properties");
+//                if(!videoProps.isNull()){
+//                    Double width = videoProps.get("width").doubleValue();
+//                    Double height = videoProps.get("height").doubleValue();
+//                    Double aspectRatio = videoProps.get("aspect_ratio").doubleValue();
+//
+//                    if(!width.isNaN())
+//                        widenAsset.setWidth(width);
+//                    if(!height.isNaN())
+//                        widenAsset.setHeight(height);
+//                    if(!aspectRatio.isNaN())
+//                        widenAsset.setAspectRatio(aspectRatio);
+//                }
+
+                break;
+            case FILE_TYPE_PDF:
+                widenAsset.setJahiaNodeType(CONTENT_TYPE_PDF);
+
+                JsonNode html5ViewerNode = widenNode.get("embeds").get("document_html5_viewer");
+                JsonNode viewerNode = widenNode.get("embeds").get("document_viewer");
+                JsonNode thumbnailNode = widenNode.get("embeds").get("document_thumbnail");
+                JsonNode originalNode = widenNode.get("embeds").get("original");
+
+                if(!html5ViewerNode.isNull())
+                    widenAsset.addProperties(PREFIX+"viewerHtml5",html5ViewerNode.get("url").textValue());
+//                    widenAsset.setViewerHtml5(html5ViewerNode.get("url").textValue());
+
+                if(!viewerNode.isNull())
+                    widenAsset.addProperties(PREFIX+"viewer",viewerNode.get("url").textValue());
+//                    widenAsset.setViewer(viewerNode.get("url").textValue());
+
+                if(!thumbnailNode.isNull())
+                    widenAsset.addProperties(PREFIX+"docThumbnail",thumbnailNode.get("url").textValue());
+//                    widenAsset.setDocThumbnail(thumbnailNode.get("url").textValue());
+
+                if(!originalNode.isNull()) {
+                    widenAsset.addProperties(PREFIX+"docURL",originalNode.get("url").textValue());
+                    widenAsset.addProperties(PREFIX+"docHTMLLink",originalNode.get("html").textValue());
+//                    widenAsset.setDocURL(originalNode.get("url").textValue());
+//                    widenAsset.setDocHTMLLink(originalNode.get("html").textValue());
+                }
+                break;
+            default:
+                widenAsset.setJahiaNodeType(CONTENT_TYPE_DOC);
+                break;
+        }
+
+
+        return widenAsset;
+    }
+
+    private void extendFileProps(JsonNode filePropsNode, WidenAsset widenAsset, String propsKey){
+        if(propsKey != null && propsKey.length() !=0){
+            JsonNode complementNode = filePropsNode.get(propsKey);
+            if(!complementNode.isNull()){
+                Double width = complementNode.get("width").doubleValue();
+                Double height = complementNode.get("height").doubleValue();
+                Double aspectRatio = complementNode.get("aspect_ratio").doubleValue();
+
+                if(!width.isNaN())
+                    widenAsset.addProperties(PREFIX+"width",width);
+//                    widenAsset.setWidth(width);
+                if(!height.isNaN())
+                    widenAsset.addProperties(PREFIX+"height",height);
+//                    widenAsset.setHeight(height);
+                if(!aspectRatio.isNaN())
+                    widenAsset.addProperties(PREFIX+"aspectRatio",aspectRatio);
+//                    widenAsset.setAspectRatio(aspectRatio);
+            }
+        }
+    }
+}
