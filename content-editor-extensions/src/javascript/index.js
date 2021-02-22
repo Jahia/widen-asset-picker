2330d58131e2e2247993a080d6c4a02340203dca
Hervé Duchesne <hduchesne@jahia.com>
2021-02-22 15:41:06 +0100

    rename callback registration event to prevent conflict with content-editor-extension

--- content-editor-extensions/src/javascript/index.js
+++ content-editor-extensions/src/javascript/index.js
@@ -1,7 +1,7 @@
 import {registry} from '@jahia/ui-extender';
 // import register from './ContentEditorExtensions.register';
 
-registry.add('callback', 'contentEditorExtensions', {
+registry.add('callback', 'widenPickerEditor', {
     targets: ['jahiaApp-init:20'],
     // callback: register
     callback: () => import('./ContentEditorExtensions.register')
